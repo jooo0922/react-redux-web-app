72a220575e1d4133323d91bbd71fa2d7bff185ac
jooo0922 <2014102458@khu.ac.kr>
2021-10-04 07:16:27 +0900

    refactor: Change UI to components

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        World Wide WEB
+      </header>
+    );
+  }
+}
+
+/**
+ * 꿀팁!
+ *
+ * 이전에 깔았던 리액트 snippets 익스텐션 덕분에
+ * 'rcc' 라고 단축키를 치면 리액트와 관련된 기본 코드들
+ * (리액트, 컴포넌트 클래스 import, 컴포넌트 클래스 기본 코드)등을
+ * 알아서 자동으로 세팅해 줌.
+ *
+ * 리액트로 애플리케이션 만들 때 참고할 것!
+ */
