46d29c36c46e331a8cc244307fae6892f9b310d6
jooo0922 <2014102458@khu.ac.kr>
2021-10-04 07:06:00 +0900

    feat: Add UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>WEB</h1>
+        World Wide WEB
       </header>
+      <nav>
+        <ol>
+          <li>
+            <a href="html.html">HTML</a>
+          </li>
+          <li>
+            <a href="css.html">CSS</a>
+          </li>
+          <li>
+            <a href="javascript.html">JavaScript</a>
+          </li>
+        </ol>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB.
+      </article>
     </div>
   );
 }
 
 export default App;
+
+/**
+ * 항상 이런 예제를 만들 때 a태그 안에 들어가는 href는 그냥 가짜 주소임.
+ */
